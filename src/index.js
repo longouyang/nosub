10b8c67e6f8740f269d9fb328078e6a6ed4723c2
Long Ouyang <longouyang@gmail.com>
2018-01-26 18:01:27 -0800

    new cosub settings spec + batch mode creation

--- src/index.js
+++ src/index.js
@@ -25,11 +25,8 @@ var getClient = function(_opts) {
 }
 
 // TODO? in addition to command-line and stdin interfaces, also allow programmatic access
-function create(settings, _options) {
-  var options = _.defaults(options || {},
-                           {environment: 'sandbox',
-                            batch: false
-                           });
+function create(settings) {
+  // TODO: if we already created this hit (in non-batch-mode, don't allow cosub create)
 
   var unitsToSeconds = {second: 1, minute: 60, hour: 3600, day: 86400, week: 604800};
 
@@ -101,41 +98,22 @@ function create(settings, _options) {
 
   var externalQuestion =
 `<ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd">
-  <ExternalURL>${settings.url}</ExternalURL>
-  <FrameHeight>${settings.frame_height}</FrameHeight>
+  <ExternalURL>${settings.Url}</ExternalURL>
+  <FrameHeight>${settings.FrameHeight}</FrameHeight>
 </ExternalQuestion>`;
 
-  var allParams = _.extend({}, settings, answers, {question: externalQuestion})
+  var allParams = _.extend({}, settings, answers, {Question: externalQuestion})
 
-  var turkParams = _ .omit(allParams, 'url', 'environment', 'frame_height');
-
-  // TODO: break backwards-compatibility, create new spec for v2
-  // (add a key for cosub version and also write a utility function to emit a json spec that works directly with aws-shell or aws-sdk-js)
-
-  var renames = {'assignment_duration': 'assignment_duration_in_seconds',
-                 'qualifications': 'qualification_requirements',
-                 'duration': 'lifetime_in_seconds',
-                 'assignment_duration': 'assignment_duration_in_seconds',
-                 'auto_approval_delay': 'auto_approval_delay_in_seconds',
-                 'assignments': 'max_assignments',
-                 'qualifications': 'qualification_requirements'
-                };
+  var renames = {
+    'duration': 'LifetimeInSeconds',
+    'assignments': 'MaxAssignments',
+    'AssignmentDuration': 'AssignmentDurationInSeconds',
+    'AutoApprovalDelay': 'AutoApprovalDelayInSeconds'
+  };
 
   function renameOldKey(_k) {
     var key = _.has(renames, _k) ? renames[_k] : _k;
-    var chars = key.split('');
-    // capitalize first character and any character following an underscore
-    var capitalize = true;
-    for(var i = 0; i < chars.length; i++) {
-      if (capitalize) {
-        chars[i] = chars[i].toUpperCase()
-        capitalize = false
-      }
-      if (chars[i] == '_') {
-        capitalize = true
-      }
-    }
-    return _.without(chars, '_').join('')
+    return key
   }
 
   function renameOldKeys(dict) {
@@ -151,21 +129,94 @@ function create(settings, _options) {
     return _.chain(newKeys).zip(newValues).fromPairs().value()
   }
 
-  turkParams = renameOldKeys(turkParams);
+  var turkParams = _.pick(renameOldKeys(allParams),
+                          ['Title', 'Description', 'Keywords', 'AssignmentDurationInSeconds',
+                           'AutoApprovalDelayInSeconds', 'LifetimeInSeconds', 'Reward',
+                           'QualificationRequirements', 'Question', 'MaxAssignments'
+                          ]);
   turkParams.AssignmentDurationInSeconds = extractDuration(turkParams.AssignmentDurationInSeconds)
   turkParams.AutoApprovalDelayInSeconds = extractDuration(turkParams.AutoApprovalDelayInSeconds)
   turkParams.Reward = turkParams.Reward + ""
 
-  var mtc = getClient(answers);
+  if (allParams.Batch) {
+    createBatch(turkParams, answers.environment)
+  } else {
+    createSingle(turkParams, answers.environment)
+  }
+
+}
+
+function delay(t, v) {
+   return new Promise(function(resolve) {
+       setTimeout(resolve.bind(null, v), t)
+   });
+}
+
+function createBatch(turkParams, environment) {
+  var mtc = getClient({environment: environment});
+
+  var metadata = {};
+
+  mtc.createHITType(_.omit(turkParams, 'LifetimeInSeconds', 'Question', 'MaxAssignments')).promise()
+    .then(function(data) {
+      console.log('Created HIT Type ' + data.HITTypeId)
+
+      var n = parseInt(turkParams.MaxAssignments),
+          numBatches = Math.ceil(n / 9),
+          batchSizes = _.map(_.range(numBatches),
+                             function(i) {
+                               return i < (numBatches - 1) ? 9 :
+                                 (n % 9 == 0 ? 9 : n % 9)
+                             })
+
+      console.log('Created HITs:');
+      var promises = batchSizes.map(function(size, i) {
+        return delay(i * 500).then(
+          function() {
+            return mtc.createHITWithHITType({
+              HITTypeId: data.HITTypeId,
+              MaxAssignments: size,
+              LifetimeInSeconds: turkParams.LifetimeInSeconds,
+              Question: turkParams.Question
+            }).promise().then(function(dat) {
+              console.log(dat.HIT.HITId)
+              return dat
+            })
+          }
+        )
+      })
+      return Promise.all(promises)
+    })
+    .catch(function(err) {
+      console.log('Error')
+      console.error(err.message)
+    })
+    .then(function(data) {
+      var existingData = fs.existsSync('hit-ids.json') ? JSON.parse(fs.readFileSync('hit-ids.json')) : {}
+
+      fs.writeFileSync('hit-ids.json',
+                       JSON.stringify(_.extend({},
+                                               existingData,
+                                               _.fromPairs([[environment, data]])),
+                                      null,
+                                      1
+                                     ))
+
+    })
+}
+
+function createSingle(turkParams, environment) {
+  var mtc = getClient({environment: environment});
+
   mtc.createHIT(turkParams).promise()
     .then(function(data) {
       var hit = data.HIT
       var existingHitIds = fs.existsSync('hit-ids.json') ? JSON.parse(fs.readFileSync('hit-ids.json')) : {}
 
-      // TODO: if we already created this hit (in non-batch-mode, don't allow cosub create)
-
       fs.writeFileSync('hit-ids.json',
-                       JSON.stringify(_.extend({}, existingHitIds, _.fromPairs([[allParams.environment, hit]]))))
+                       JSON.stringify(_.extend({},
+                                               existingHitIds,
+                                               _.fromPairs([[environment, hit]]))))
 
       console.log('Created HIT ' + hit.HITId)
     })
@@ -189,6 +240,12 @@ var action  = argv['_'];
 
 if (action == 'create') {
   var settings = JSON.parse(fs.readFileSync(argv['hit-file'], 'utf8'));
+
+  if (!_.has(settings, "_cosubSpecVersion") || settings._cosubSpecVersion != 2) {
+    console.error("Error reading settings file -- you may be reusing a settings file from a previous version of cosub")
+    process.exit()
+  }
+
   // TODO: handle error
   create(settings)
 }
